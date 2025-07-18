8c3de571e24c3f412f6160f3e7fb6d0b06f531c2
nikhilmantri0902 <nikhil.mantri1999@gmail.com>
2025-07-18 15:47:12 +0530

    added monitoring changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,16 +4,78 @@ import (
 	"cold_emailer/api"
 	"cold_emailer/constants"
 	"cold_emailer/db"
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"google.golang.org/grpc/credentials"
 )
 
+var (
+	serviceName  = os.Getenv("SERVICE_NAME")
+	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	insecure     = os.Getenv("INSECURE_MODE")
+)
+
+func initTracer() func(context.Context) error {
+
+	var secureOption otlptracegrpc.Option
+
+	if strings.ToLower(insecure) == "false" || insecure == "0" || strings.ToLower(insecure) == "f" {
+		secureOption = otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	} else {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to create exporter: %v", err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("Could not set resources: %v", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
+
 func main() {
+
+	cleanup := initTracer()
+	defer cleanup(context.Background())
+
 	// to add migration file:
 	// go run cmd/server/main.go --generate-migration profile_info_resume_path
 	migrationSuffix := flag.String("generate-migration", "", "Generate a new migration file with the given suffix and exit")
@@ -46,6 +108,8 @@ func main() {
 
 	// Initialize Gin
 	r := gin.Default()
+	// pass the otelgin middleware
+	r.Use(otelgin.Middleware(serviceName))
 
 	// Example health route
 	r.GET("/health", func(c *gin.Context) {
